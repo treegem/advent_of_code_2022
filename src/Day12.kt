6577d60ebaaa490e5bf5c41632851dd378c014c5
gbrown <georg.braunbeck@iits-consulting.de>
2022-12-14 10:34:52 +0100

    day 12 both puzzles solved

--- src/Day12.kt
+++ src/Day12.kt
@@ -1,40 +1,98 @@
 import common.Position
 import common.readInput
+import kotlin.math.min
+
+// i am sorry for the mess, but i fell behind in schedule and will not refactor or optimize this code
+// still hoping to catch up
 
 fun main() {
 
-    fun part1(input: List<String>): Int {
+    fun part1(input: List<String>): Long {
         val charCodeArray = input.to2dArray()
         val startingPosition = charCodeArray.startingPosition
         val targetPosition = charCodeArray.targetPosition
-        val routes = listOf<List<Position>>()
-        val heights = charCodeArray.convertToHeights()
-        val visitedPositions = mutableListOf(startingPosition)
-        var currentRoute =
-        var currentPosition = startingPosition
-        while (currentPosition != targetPosition) {
-            val nextPossiblePositions = mutableListOf<Position>()
-            val xs = (currentPosition.x -1 .. currentPosition.x + 1)
-            val ys = (currentPosition.y -1 .. currentPosition.y + 1)
-        }
-
-        return input.size
+        val allNodes = createNodes(charCodeArray)
+        val startingNode = allNodes.getByPosition(startingPosition)
+        return calculateDistanceToTargetPosition(startingNode, targetPosition, allNodes)
     }
 
-    fun part2(input: List<String>): Int {
-        return input.size
+    fun part2(input: List<String>): Long {
+        val charCodeArray = input.to2dArray()
+        val targetPosition = charCodeArray.targetPosition
+        val allNodes = createNodes(charCodeArray)
+
+        return allNodes
+            .filter { it.height == 'a'.code }
+            .minOfOrNull {
+                calculateDistanceToTargetPosition(it, targetPosition, allNodes)
+                    .also { reset(allNodes) }
+            }!!
     }
 
     val day = "12"
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day${day}_test")
-    check(part1(testInput) == 31)
+    check(part1(testInput) == 31L) { part1(testInput) }
+    check(part2(testInput) == 29L) { part2(testInput) }
 
     val input = readInput("Day${day}")
     println(part1(input))
     println(part2(input))
 }
+
+private fun reset(nodes: List<Node>) {
+    nodes.forEach { it.apply { hasBeenVisited = false } }
+}
+
+private fun calculateDistanceToTargetPosition(
+    startingNode: Node,
+    targetPosition: Position,
+    allNodes: List<Node>,
+): Long {
+    println("-------------")
+    println("startingNode: ${startingNode.position}")
+    println("-------------")
+    val unvisitedNodes = allNodes.toMutableList()
+    var currentNode = startingNode.apply { distanceToStart = 0 }
+    while (!targetHasBeenReached(allNodes, targetPosition) && existsReachableUnvisitedNode(allNodes)) {
+        unvisitedNodes.remove(currentNode)
+        currentNode.apply { hasBeenVisited = true }
+        val relevantNeighbors =
+            unvisitedNodes
+                .filter { it.isDirectNeighborOf(currentNode) }
+                .filter { it.isClimbableFrom(currentNode) }
+        relevantNeighbors.forEach {
+            it.apply { distanceToStart = min(distanceToStart, currentNode.distanceToStart + 1) }
+        }
+        currentNode = unvisitedNodes.minByOrNull { it.distanceToStart } ?: break
+    }
+    return allNodes
+        .getByPosition(targetPosition)
+        .distanceToStart
+}
+
+private fun createNodes(charCodeArray: Array<IntArray>): List<Node> {
+    val heights = charCodeArray.convertToHeights()
+    return (charCodeArray.indices).flatMap { row ->
+        charCodeArray[0].indices.map { column ->
+            Node(
+                position = Position(x = column, y = row),
+                height = heights[row][column]
+            )
+        }
+    }
+}
+
+private fun targetHasBeenReached(allNodes: List<Node>, targetPosition: Position) =
+    allNodes.getByPosition(targetPosition).hasBeenVisited
+
+private fun existsReachableUnvisitedNode(allNodes: List<Node>) =
+    allNodes
+        .filterNot { it.hasBeenVisited }
+        .minOf { it.distanceToStart } < Long.MAX_VALUE
+
+private fun List<Node>.getByPosition(position: Position) = this.single { it.position == position }
 
 private fun List<String>.to2dArray(): Array<IntArray> =
     this.map { it.toCharArray().toTypedArray() }
@@ -43,7 +101,7 @@ private fun List<String>.to2dArray(): Array<IntArray> =
 
 private fun Array<IntArray>.getPositionOf(target: Int) = this.mapIndexedNotNull() { index, row ->
     if (row.contains(target)) {
-        Position(index, row.indexOf(target))
+        Position(y = index, x = row.indexOf(target))
     } else null
 }.single()
 
@@ -56,4 +114,24 @@ private val Array<IntArray>.targetPosition: Position
 private fun Array<IntArray>.convertToHeights() = this.apply {
     this[this.startingPosition.y][this.startingPosition.x] = 'a'.code
     this[this.targetPosition.y][this.targetPosition.x] = 'z'.code
+}
+
+private data class Node(
+    val position: Position,
+    val height: Int,
+) {
+    var hasBeenVisited: Boolean = false
+    var distanceToStart: Long = Long.MAX_VALUE
+
+    fun isDirectNeighborOf(other: Node) =
+        (this.position.x == other.position.x && this.position.y in listOf(
+            other.position.y - 1,
+            other.position.y + 1
+        )
+                || this.position.y == other.position.y && this.position.x in listOf(
+            other.position.x - 1,
+            other.position.x + 1
+        ))
+
+    fun isClimbableFrom(other: Node) = this.height <= other.height + 1
 }
